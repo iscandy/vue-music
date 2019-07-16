ae499b5d7d6b65b4efa3578968377a48e5fd69e0
candy <834772066@qq.com>
2019-07-16 14:22:44 +0800

    加载更多

--- src/components/suggest/suggest.vue
+++ src/components/suggest/suggest.vue
@@ -9,10 +9,15 @@
                     <p  class="text" v-html="getDisplayName(item)"></p>
                 </div>
                 </li>
-                <!-- <li class="suggest-list-loading" v-show="isLoading">
+
+                <li class="suggest-list-loading" v-show="hasMore">
                     <loading :title=title></loading>
-                </li> -->
+                </li>
             </ul>
+            <!-- 展示暂无数据-->
+            <div v-show="!hasMore && !list.length" class="no-result-wrapper">
+                <noResult title="暂无数据"></noResult>
+            </div>
         </scroll>   
 </template>
 
@@ -22,18 +27,20 @@ import {ERR_OK} from '../../api/config'
 const PAGESIZE = 20 //抓取数据一页有多少数据
 const SINGER_TYPE="singer"
 
-
-
 import createSong from '../../common/js/song'
 import {getMusicVkey} from '../../api/singer'
 //上拉加载功能
 import scroll from 'components/base/scroll/scroll'
 import loading from 'components/base/loading/loading'
+//展示暂无数据
+import noResult from 'components/base/no-result/no-result'
+
 
 export default {
     components:{
         scroll,
-        loading
+        loading,
+        noResult
     },
     props:{
         query:{
@@ -51,58 +58,41 @@ export default {
             list:[],//当前的列表的数据
             title:'',
             pullup:true,
-            hasMore:true,//是否还可展示更多的数据
-            zhida:{},//zhida
-            searchSongs:[],//song类集合
-            pushOver:[],//标记是否异步结束了
-            firstList:[]
+            hasMore:false,//是否还可展示更多的数据
         }
     },
     methods:{
         //发送请求
-        search(newQuery){
+        async search(newQuery){
+            this.pageIndex=1;
+            this.list=[];
             //关键字,当前页(在data中维护当前页),一页多少条,是否展示歌手(根据props传递过来)
-            //异步1
-            search(newQuery,this.pageIndex,PAGESIZE,this.showSinger).then(res=>{
-                if(res.code==ERR_OK){
-                    this.zhida = res.data.zhida;//将zhida存起来
-                    this.firstList=res.data.song.list;
-                    this.searchSongs =this._nomalizeSong(res.data.song.list)//把song列表部分的存起来，并且转化成一个song类的集合
+            let result=await search(newQuery,this.pageIndex,PAGESIZE,this.showSinger)
+            if(result.code==ERR_OK){
+                if(result.data.zhida && result.data.zhida.albumid){
+                    this.list.push({...result.data.zhida,...{type:SINGER_TYPE}});
                 }
-            })
+               // console.log(result.data.song.list)
+                if(result.data.song.list){
+                    let res=await this._nomalizeSong(result.data.song.list);
+                    this.list=this.list.concat(res)
+                    this._checkMore(result.data.song);
+                }
+            }
         },
          //将data.song.list转化成一个歌曲类
-        _nomalizeSong(list){
-             let ret=[];
-             let pushIndex =0  //判断是否是最最后一次push 
-             list.forEach(musicData => {
-                if(musicData.songid && musicData.songmid){
-                    getMusicVkey(musicData.songmid).then(res=>{
-                        if(res.code==ERR_OK){
-                            ret.push(createSong(musicData,res.data.items[0].vkey));
-                        }
-                        pushIndex++
-                        this.pushOver = list.length===pushIndex//是否加载完成了
-                    })
+       async  _nomalizeSong(list){
+            let ret=[];
+            for(let item of list){
+                if(item.songid && item.songmid){
+                    let result=await getMusicVkey(item.songmid);
+                    if(result.code==ERR_OK){
+                        ret.push(createSong(item,result.data.items[0].vkey))
+                    }
                 }
-             });
-             return ret
-         },
-        _genResult(zhida,newValue){
-            let ret = []
-            //push歌手进空数组
-            if(zhida.singerid){
-                ret.push({...zhida,...{type:SINGER_TYPE}})  //es6语法，对象拓展符。等同于object.assign()新建对象
             }
-            //合并歌曲进数组
-            if (newValue) {
-                ret = ret.concat(newValue)
-            }
-            let last_list=this.list.slice(0);
-            this.list=last_list.concat(ret.slice(0));
-           // this.list =this.list.push(ret)
+            return ret
         },
-       
         //做图标处理
         getIconCls(item){
             if(item.type==SINGER_TYPE){
@@ -119,25 +109,27 @@ export default {
                  return `${item.name}-${item.singer}`
             }
         },
-        searchMore(){
-           if(!this.hasMore){//是否还可以展示更多的数据
-               return
-           }
-           this.pageIndex++;//当前页+1
-          search(this.query,this.pageIndex,PAGESIZE,this.showSinger).then(res=>{
-                if(res.code==ERR_OK){
-                   //将下一页数据拼接上上一条的数据
-                   this.pushOver=false
-                   this.searchSongs =this._nomalizeSong(res.data.song.list)
+        async searchMore(){//加载更多
+            if(!this.hasMore){
+                return 
+            }
+            this.pageIndex++;
+            //关键字,当前页(在data中维护当前页),一页多少条,是否展示歌手(根据props传递过来)
+            let result=await search(this.query,this.pageIndex,PAGESIZE,this.showSinger)
+            if(result.code==ERR_OK){
+                if(result.data.song.list){
+                    let res=await this._nomalizeSong(result.data.song.list);
+                    this.list=this.list.concat(res)
+                    this._checkMore(result.data.song);
                 }
-            })
-
+            }
         },
         _checkMore(data){
-            console.log(data);
-            //当当前的this.list.length为空，并且  当页的条数+当前页*一页多少条
-            if(!this.list.length || (data.curnum+data.curpage*PAGESIZE) >=data.totalnum){
-                 this.hasMore = false
+           // console.log("当前页："+data.curpage,"当前条数"+data.curnum,'总数'+data.totalnum);
+            if(!data.list || (data.curpage-1)*PAGESIZE+data.curnum>=data.totalnum){
+                this.hasMore=false
+            }else{
+                this.hasMore=true
             }
         }
     },
@@ -145,12 +137,6 @@ export default {
       query(newQuery) {
         //监听到query的变化，发送请求
         this.search(newQuery);
-      },
-      searchSongs(newValue){
-           //判断异步完成后合并zhida和当前的song类数组
-            if(this.pushOver){     
-                this._genResult(this.zhida,newValue)
-            }   
       }
     }
 }
